022c084b30335ca86fcddb86c4b5693e7ba7023d
Subhadip Biswas <bsubhadip114@gmail.com>
2024-05-18 19:35:02 +0530

    add

---
+++ hhh.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+   int a,b,c;
+   printf("Enter first number");
+   scanf("%d",&a);
+   printf("Enter second number");
+   scanf("%d",&b);
+   c=a+b;
+   printf("The result is %d",c);
+    return 0;
+}
